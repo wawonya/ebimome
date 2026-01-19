785dba1276c2b4f158c5f1a1ade56e4983a24949
wawonya <163826700+wawonya@users.noreply.github.com>
2026-01-19 19:41:36 +0900

    Add service worker for caching video files

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "video-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./movie.mp4"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
