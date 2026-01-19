604236d62db941be3d59c6e402707b94ece1624e
wawonya <163826700+wawonya@users.noreply.github.com>
2026-01-19 19:43:36 +0900

    Simplify cache handling in service worker

--- public/sw.js
+++ public/sw.js
@@ -7,16 +7,12 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
